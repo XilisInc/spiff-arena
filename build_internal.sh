cb3d73bc4062bdc20f6e5870bc8c6d9fb8fa1861
David Eklund <david.eklund@xilis.net>
2024-06-10 19:56:26 -0400

    Add '--platform linux/amd64' to docker build

--- build_internal.sh
+++ build_internal.sh
@@ -2,9 +2,9 @@
 set -euxo pipefail
 currentcommit=$(git rev-parse --short HEAD)
 echo $currentcommit
-docker build ./connector-proxy-demo -t 607182506347.dkr.ecr.us-east-1.amazonaws.com/spiff:connector-proxy-demo-${currentcommit}
-docker build ./spiffworkflow-frontend -t 607182506347.dkr.ecr.us-east-1.amazonaws.com/spiff:spiffworkflow-frontend-${currentcommit}
-docker build ./spiffworkflow-backend -t 607182506347.dkr.ecr.us-east-1.amazonaws.com/spiff:spiffworkflow-backend-${currentcommit}
+docker build ./connector-proxy-demo -t 607182506347.dkr.ecr.us-east-1.amazonaws.com/spiff:connector-proxy-demo-${currentcommit} --platform linux/amd64
+docker build ./spiffworkflow-frontend -t 607182506347.dkr.ecr.us-east-1.amazonaws.com/spiff:spiffworkflow-frontend-${currentcommit} --platform linux/amd64
+docker build ./spiffworkflow-backend -t 607182506347.dkr.ecr.us-east-1.amazonaws.com/spiff:spiffworkflow-backend-${currentcommit} --platform linux/amd64
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 607182506347.dkr.ecr.us-east-1.amazonaws.com
 docker push 607182506347.dkr.ecr.us-east-1.amazonaws.com/spiff:connector-proxy-demo-${currentcommit}
 docker push 607182506347.dkr.ecr.us-east-1.amazonaws.com/spiff:spiffworkflow-frontend-${currentcommit}
